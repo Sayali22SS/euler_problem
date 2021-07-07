4b11e7266fcc683e06e45237c4358d873aa17672
Sayali22SS <84305191+Sayali22SS@users.noreply.github.com>
2021-07-07 12:32:38 +0530

    Delete p9.py

--- p9.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    
-    
-    for a in range(1, 400):
-        for b in range(1, 400):
-            c = (1000 - a) - b
-            if a < b < c:
-                if a**2 + b**2 == c**2:
-                    print(a * b * c)
