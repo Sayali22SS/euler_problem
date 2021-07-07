27a94b4abbc079362e437d89cb48141ac94726f8
Sayali22SS <84305191+Sayali22SS@users.noreply.github.com>
2021-07-07 12:32:29 +0530

    Delete p8.py

--- p8.py
+++
@@ -1,7 +0,0 @@
-def largest_product_series(n, series):
-    series = str(series)
-    largest = 0
-    for i in range(0,1000-n):
-         temp = np.prod([int(series[j]) for j in range(i,n+i)])
-         largest = max(temp, largest)
-    return largest
